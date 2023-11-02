7a2929bb723a993a54319630c5f5d052662aecf9
george <r.george.varghese@gmail.com>
2023-11-02 08:41:00 -0500

    implemented RetrievalQA chain

---
+++ ingestion.py
@@ -0,0 +1,39 @@
+import os
+from langchain.document_loaders import ReadTheDocsLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+
+from consts import INDEX_NAME
+
+pinecone.init(
+    api_key=os.environ["PINECONE_API_KEY"],
+    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
+)
+
+
+def ingest_docs() -> None:
+    loader = ReadTheDocsLoader(path="langchain-docs/api.python.langchain.com/en/latest")
+    raw_documents = loader.load()
+    print(f"loaded{len(raw_documents) } documents")
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=400, chunk_overlap=50, separators=["\n\n", "\n", " ", ""]
+    )
+    documents = text_splitter.split_documents(documents=raw_documents)
+    print(f"Splitted into{len(documents)} chunks")
+
+    for doc in documents:
+        old_path = doc.metadata["source"]
+        new_url = old_path.replace("langchain-docs", "https:/")
+        doc.metadata.update({"source": new_url})
+    print(f"Going to insert {len(documents)} to Pinecone")
+    embeddings = OpenAIEmbeddings()
+    Pinecone.from_documents(
+        documents=documents, embedding=embeddings, index_name=INDEX_NAME
+    )
+    print("**** Added Vector in vectorstore")
+
+
+if __name__ == "__main__":
+    ingest_docs()
